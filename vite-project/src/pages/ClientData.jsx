162368058d69e760c7cf9f19073148626c49e495
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2025-06-16 10:54:00 +0300

    Update ClientData.jsx

--- vite-project/src/pages/ClientData.jsx
+++ vite-project/src/pages/ClientData.jsx
@@ -4,9 +4,12 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import { useState } from "react";
+import React from "react";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import Collapse from "@mui/material/Collapse";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
 const ClientData = () => {
   const [value, setValue] = useState("1");
@@ -14,6 +17,11 @@ const ClientData = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  // Form state variables
+  // These will hold the values for the form fields
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
 
   return (
     <>
@@ -74,19 +82,25 @@ const ClientData = () => {
             {" "}
             Client Details{" "}
           </Typography>
-          <Collapse in={value === "1"}>
-            <InputBox
-              id="f-name"
-              label="First Name"
-              variant="outlined"
-              required
-            />
-            <InputBox
-              id="l-name"
-              label="Last Name"
-              variant="outlined"
-              required
-            />
+          <Collapse in={value === "1"} unmountOnExit mountOnEnter>
+            <Stack spacing={2} direction="column">
+              <TextField
+                label="First Name"
+                variant="outlined"
+                value={first_name}
+                onChange={(e) => setFirstName(e.target.value)}
+                id="f-name"
+                required
+              />
+              <TextField
+                label="Last Name"
+                variant="outlined"
+                value={last_name}
+                onChange={(e) => setLastName(e.target.value)}
+                id="l-name"
+                required
+              />
+            </Stack>
           </Collapse>
           <Collapse in={value === "2"}>
             <InputBox
