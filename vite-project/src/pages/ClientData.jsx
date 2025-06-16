952fc3ad6e24214333eb869caaf4730098315f3a
Michemor <yatoijema@gmail.com>
2025-06-16 09:56:33 +0300

    tab input

--- vite-project/src/pages/ClientData.jsx
+++ vite-project/src/pages/ClientData.jsx
@@ -2,11 +2,22 @@ import FormControl from "@mui/material/FormControl";
 import InputBox from "../components/InputBox";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import { useState } from "react";
+import Tab from "@mui/material/Tab";
 
 
 const ClientData = () => {
+
+    const [value, setValue] = useState('1');
+
+   const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
+
     return (
         <div>
+            <TabContext value={value}>
             <Box sx={{
                 display: 'flex',
                 flexDirection: 'column',
@@ -19,12 +30,33 @@ const ClientData = () => {
                 fontWeight: 'bold',
                 fontSize: 20,
                 color: '#03a9f4'
-            }}> Client Details </Typography>
-            <FormControl
+            }}> Sales Office </Typography>
+
+            <TabList onChange={handleChange} aria-label="Client Details">
+                <Tab label="Client Details" value="1" />
+                <Tab label="Meeting Details" value="2" />
+                <Tab label="Office Details" value="3" />
+                <Tab label="Client Inquries " value="4" />
+
+            </TabList>
+            <Divider middle aria-hidden='true' sx={{
+                mr: 5,
+                ml: 5,
+            }} />
+           
+         <FormControl
             title="Client Details"
             sx={{
-                m: 4,
+                ml: 4,
+                mr: 4,
             }}>
+                <TabPanel value="1"> 
+                    <Typography sx={{
+                mt: 5,
+                fontWeight: 'bold',
+                fontSize: 20,
+                color: '#03a9f4'
+            }}> Client Details </Typography>
             <InputBox 
             id='f-name'
             label='First Name'
@@ -50,13 +82,27 @@ const ClientData = () => {
             required
             />
             <InputBox 
+            id='job-title'
+            label='Job Title'
+            variant="outlined"
+            required
+            />
+             <Divider middle aria-hidden='true' sx={{
+                mr: 5,
+                ml: 5,
+            }} />
+           
+            <InputBox 
             id='m-date'
             label='Meeting Date'
             variant="outlined"
             required
             />
+                </TabPanel>
+            
           </FormControl>
             </Box>
+            </TabContext>
         </div>
     );
 }
